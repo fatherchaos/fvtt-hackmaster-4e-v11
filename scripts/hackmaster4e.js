d6604dd961b5eec2866c4a933b3e140d55580cf6
drplote <drplote@gmail.com>
2022-06-22 00:12:36 -0400

    Added AC attributes for Token Mold support

--- scripts/hackmaster4e.js
+++ scripts/hackmaster4e.js
@@ -15,11 +15,32 @@ function overrideStatBonuses(){
   })(), 'OVERRIDE');
 }
 
+// This exists primarily for Token Mold support.
+function addAcFieldsToActorDefinitions(){
+  let armorClass = {
+     "normal": 10,
+     "rear": 10,
+     "touch": 10,
+     "shieldless": 10,
+     "nodex": 10,
+     "acranged": 10,
+     "acrangedfront": 10,
+     "acrangedrear": 10,
+     "armormelee": 10,
+     "armormeleefront": 10,
+     "armormeleerear": 10
+  };
+
+  game.system.model.Actor.character.armorClass = armorClass;
+  game.system.model.Actor.npc.armorClass = armorClass;
+}
+
 Hooks.once('init', function() {
   console.log(`Initializing "${MODULE_NAME}"`);
 
   CONFIG.Hackmaster = Hackmaster;
 
   overrideStatBonuses();
+  addAcFieldsToActorDefinitions();
 });
 
