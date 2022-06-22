1db44a74640686ceccb732c867d6ee2c1319f70a
drplote <drplote@gmail.com>
2022-06-21 22:09:50 -0400

    minor refactoring

--- scripts/hackmaster4e.js
+++ scripts/hackmaster4e.js
@@ -1,19 +1,25 @@
 import { libWrapper } from './shim.js';
 import { HackmasterActor } from './hackmaster-actor.js';
-import { Hackmaster } from './config.js';``
+import { Hackmaster } from './config.js';
+
+const MODULE_NAME = "Hackmaster 4th Edition";
+const MODULE_ID = "hackmaster-4e";
+
+function overrideStatBonuses(){
+
+  CONFIG.OSRIC.constitutionTable = Hackmaster.ConstitutionTable;
+  libWrapper.register(MODULE_ID, 'CONFIG.Actor.documentClass.prototype._buildAbilityFields', (function() {
+    return function(data) {
+      HackmasterActor._buildAbilityFields(data);
+    };
+  })(), 'OVERRIDE');
+}
 
 Hooks.once('init', function() {
-  const MODULE_NAME = "Hackmaster 4th Edition";
-  const MODULE_ID = "hackmaster-4e";
   console.log(`Initializing "${MODULE_NAME}"`);
 
   CONFIG.Hackmaster = Hackmaster;
-  CONFIG.OSRIC.constitutionTable = Hackmaster.ConstitutionTable;
 
-  libWrapper.register(MODULE_ID, 'CONFIG.Actor.documentClass.prototype._buildAbilityFields', (function(data) {
-      return function(data) {
-        HackmasterActor._buildAbilityFields(data);
-      }
-  })(), 'OVERRIDE');
+  overrideStatBonuses();
 });
 
